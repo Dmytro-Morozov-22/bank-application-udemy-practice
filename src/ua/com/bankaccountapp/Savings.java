8dfe8fea4d0887bb7805be4acc6481e378ec0875
dmytromorozovtiras <dmytro.morozov.tiras@gmail.com>
2023-10-12 15:36:40 +0300

    Methods increamentAccountNumber were added to Checking and Savings classes

--- src/ua/com/bankaccountapp/Savings.java
+++ src/ua/com/bankaccountapp/Savings.java
@@ -9,9 +9,14 @@ public class Savings extends Account {
 	// Constructor to initialize settings for the Savings properties
 	public Savings(String name, String socialSecurityNumber, double initDeposit) {
 		super(name, socialSecurityNumber, initDeposit);
+		increamentAccountNumber();
+	}
+	
+	private void increamentAccountNumber() {
 		accountNumber = "1" + accountNumber;
 		System.err.println("Accaunt number: " + this.accountNumber);
 		System.out.println("NEW SAVINGS ACCOUNT");
 	}
+	
 	// List any methods specific to savings account
 }
